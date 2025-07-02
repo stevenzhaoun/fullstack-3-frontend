3667c0a7ab8dee8d8d2c3ed6b535d31ded88d2f0
Steven Zhao <stevenzhao0819@gmail.com>
2025-07-01 18:39:33 -0700

    app layout & routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import './App.css'
-import Todo from './Todo'
+import Layout from './components/Layout'
+import { Routes, Route } from 'react-router'
+
+function Dashboard() {
+  return <div>Dashboard</div>
+}
+
+function Users() {
+  return <div>Users</div>
+}
+
+function Roles() {
+  return <div>Roles</div>
+}
+
 function App() {
 
   return (
     <>
-      <Todo />
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="users" element={<Users />} />
+          <Route path="roles" element={<Roles />} />
+        </Route>
+      </Routes>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <CssBaseline />
+      <App />
+    </BrowserRouter>
+  </StrictMode >,
 )
